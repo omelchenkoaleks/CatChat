b68fe6bc33c6a8d5204d35ee26d8ba6d3935e21c
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-11 14:38:36 +0300

    не работает - разобраться...

--- app/src/main/java/com/omelchenkoaleks/catchat/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/catchat/MainActivity.java
@@ -1,7 +1,10 @@
 package com.omelchenkoaleks.catchat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.content_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
