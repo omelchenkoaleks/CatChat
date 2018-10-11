0333b3b665ee53a8106e980152b5f37af7844a61
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-11 13:04:30 +0300

    добавили зависимость материал дизайн и создали чертые фрагмента с макетами как заготовки

---
+++ app/src/main/java/com/omelchenkoaleks/catchat/SentItemsFragment.java
@@ -0,0 +1,22 @@
+package com.omelchenkoaleks.catchat;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SentItemsFragment extends Fragment {
+
+    public SentItemsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sent_items, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/omelchenkoaleks/catchat/TrashFragment.java
@@ -0,0 +1,23 @@
+package com.omelchenkoaleks.catchat;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrashFragment extends Fragment {
+
+
+    public TrashFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trash, container, false);
+    }
+
+}
