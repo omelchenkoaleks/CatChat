0d26dc7dc4613e5f630042c940406ed6f2b5f367
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-12 07:45:15 +0300

    навигационная панель работоспособна

--- app/src/main/java/com/omelchenkoaleks/catchat/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/catchat/MainActivity.java
@@ -1,9 +1,14 @@
 package com.omelchenkoaleks.catchat;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -23,10 +28,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.content_frame, fragment);
         fragmentTransaction.commit();
+
+        // добавляем кнопку вызова панели
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout, toolbar, R.string.nav_oper_drawer, R.string.nav_close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // регистрируем слушателя MainActivity
+        // 1 - получаем ссылку
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        // 2 - вызываем метод
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    // этот метод вызывается тогда, когда пользователь выбирает команду на навигационной панели
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        // код этого метода должен отобразить экран, соответствующий выбранной команде
+
+        int id = menuItem.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch (id) {
+            case R.id.nav_drafts:
+                fragment = new DraftsFragment();
+                break;
+            case R.id.nav_sent:
+                fragment = new SentItemsFragment();
+                break;
+            case R.id.nav_trash:
+                fragment = new TrashFragment();
+                break;
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+            case R.id.nav_feedback:
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+                default:
+                    fragment = new InboxFragment();
+        }
+
+        // отображает соответствующий фрагмент или активность
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment);
+            fragmentTransaction.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        // панель закрывается, когда пользователь выбирает одну из команд
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    // когда пользователь нажимает кнопку назад при открытой навигационной панели, панель закрывается
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
